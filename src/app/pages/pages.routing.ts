d9075933e308acfb460fbaf9416ee557b862ec9b
andres <telescopio322@gmail.com>
2020-12-03 22:05:11 -0500

    observables & rxjs

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -5,16 +5,21 @@ import { PagesComponent } from './pages.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
+import { PromesasComponent } from './promesas/promesas.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+
 
 const routes: Routes = [
     { 
         path: 'dashboard', component: PagesComponent,
         children: [
-            { path: '', component: DashboardComponent },
-            { path: 'grafica1', component: Grafica1Component },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'account-settings', component: AccountSettingsComponent }
+            { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+            { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Grafica' }  },
+            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'rxjs' } },
+            { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' }  },
+            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' }  },
+            { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes' }  }
         ]
     },  
 ];
